4eddab8e1fea805d3e9c3aca1173686d000a7b27
denislavz <user@MacBook-Pro.local>
2024-02-28 20:13:23 +0200

    Added Swagger UI

--- target/generated-sources/annotations/com/distributedappspu/cscources/mapper/StudentMapperImpl.java
+++ target/generated-sources/annotations/com/distributedappspu/cscources/mapper/StudentMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-28T16:21:54+0200",
+    date = "2024-02-28T20:11:54+0200",
     comments = "version: 1.6.0.Beta1, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
 )
 @Component
